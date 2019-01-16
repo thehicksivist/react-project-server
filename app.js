0b0e8aead4421b2db53b69612e57a9db27555450
thanhthedev <huythanhdao@gmail.com>
2019-01-16 12:20:39 +1000

    added app.js and installed express

---
+++ app.js
@@ -0,0 +1,13 @@
+// dependencies
+const express = require('express')
+const app = express()
+// connect to mlabs
+
+
+//routes
+app.get('/', function(req, res, next){
+    res.send("<h1>team4everaintclever</h1>");
+})
+
+// port 3001
+app.listen(process.env.PORT || 3001, () => console.log('Listening on http://localhost:3001'))
