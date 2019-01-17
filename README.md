0b0190c6f62aa74aa051bea04e2b505d88abc6ab
thanhthedev <huythanhdao@gmail.com>
2019-01-17 11:35:58 +1000

    added gitignore removed node modules folder

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 
 yeah boi team4everaintclever
+
+1. npm i to install dependecies
--- app.js
+++ app.js
@@ -2,13 +2,21 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
 // connect to mlabs
 
 
 //routes
 app.get('/', function(req, res, next){
     res.send("<h1>team4everaintclever</h1>");
+})
+
+//mongoose
+mongoose.connect(dbConn, (err) => {
+    if (err) {
+      console.log('Error connecting to database', err)
+    } else {
+      console.log('Connected to database!')
+    }
 })
 
 // port 3001
