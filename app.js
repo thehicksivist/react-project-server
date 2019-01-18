a005d57b499a634217f5dadc57b58281a7eac97d
thanhthedev <huythanhdao@gmail.com>
2019-01-18 13:45:04 +1000

    commented out

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.get('/', function(req, res, next){
 })
 
 //mongoose
-mongoose.connect(dbConn, (err) => {
-    if (err) {
-      console.log('Error connecting to database', err)
-    } else {
-      console.log('Connected to database!')
-    }
-})
+// mongoose.connect(dbConn, (err) => {
+//     if (err) {
+//       console.log('Error connecting to database', err)
+//     } else {
+//       console.log('Connected to database!')
+//     }
+// })
 
 // port 3001
 app.listen(process.env.PORT || 3001, () => console.log('Listening on http://localhost:3001'))
