975c1016899c7ee2adaf92a061bc79fe7770986d
thehicksivist <hicks.a.james@gmail.com>
2019-01-24 13:42:38 +1000

    added template component files

---
+++ client/src/components/Image.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Image extends Component {
+	render() {
+		return <div />;
+	}
+}
+
+export default Image;
---
+++ client/src/components/Map.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+	render() {
+		return <div>MAP</div>;
+	}
+}
+
+export default Map;
