c88edd27dd4341b4fe36128b9bea0622bbd9e325
jaouadpxy <jaouad.pxy@gmail.com>
2019-01-25 10:56:54 +1000

    Update Quote.js

--- client/src/components/Quote/Quote.js
+++ client/src/components/Quote/Quote.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Form from './Form';
 import Text from '../Text'
 
-
+// Quote suppose to be a Class i will return alot of stuff
 const Quote = () => {
 	return (
         <div>
