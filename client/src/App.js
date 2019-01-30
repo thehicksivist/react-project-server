1cdd3b8789534690bc23d529bc503d3102f829ad
=jaouad Sourour <jaouad.pxy@gmail.com>
2019-01-30 11:09:25 +1000

    added Job Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
+import './resources/styles.css';
+import Header from './components/layout/Header';
+import Featured from './components/featured';
+import Jobs from './components/jobs';
 import Footer from './components/layout/Footer';
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Navbar />
-				<h1>Five Stars Mowing</h1>
+			<div className="App" style={{ height: '1500px', background: 'cornflowerBlue' }}>
+				<Header />
+				<Featured />
+				<Jobs />
 				<Footer />
 			</div>
 		);
