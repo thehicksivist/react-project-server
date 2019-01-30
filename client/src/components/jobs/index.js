fb57fa7e45c6c5f0862a56d3a350c8aed3b428c2
=jaouad Sourour <jaouad.pxy@gmail.com>
2019-01-30 12:02:16 +1000

    activated the client folder

---
+++ client/src/components/jobs/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Jobs = () => {
+	return (
+		<div className="bck_black">
+			<div className="center_wrapper">
+				<div className="vn_wrapper">
+					<div className="vn_item">
+						<div className="vn_outer">
+							<div className="vn_inner">
+								<div className="vn_icon_square bck_red" />
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Jobs;
