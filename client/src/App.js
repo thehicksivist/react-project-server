3dfca62ce57ca15311a8133d88cd3259adaaa6f2
thehicksivist <hicks.a.james@gmail.com>
2019-01-31 13:39:35 +1000

    added navlinks and fixed merge conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,12 @@ import React, { Component } from 'react';
 import './styles/servicestyles.css'
 import './styles/carousel.css'
 import Header from './components/Header';
-import Carousel from './components/Home/Carousel';
-import Packages from './components/Home/Packages';
-import Footer from './components/Footer';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import Home from './components/Home/Home'
 import About from './components/About/About';
 import Services from './components/Services/Services';
 import Quote from './components/Quote/Quote';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
@@ -19,10 +18,9 @@ class App extends Component {
 				<div className="App" style={{ height: '1500px', background: 'cornflowerBlue' }}>
 					{
 						<div>
-							<Redirect from="/" to="/home" />
-							<Route path="/" component={Header}/>
-							<Route exact path="/home" component={Carousel}/>
-							<Route exact path="/home" component={Packages}/>	
+							<Redirect path='/' to='/home'/>
+							<Route path='/' component={Header}/>
+							<Route exact path="/home" component={Home} />
 							<Route exact path="/about" component={About} />
 							<Route exact path="/services" component={Services} />
 							<Route exact path="/freequote" component={Quote} />
