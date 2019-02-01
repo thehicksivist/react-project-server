89404e3557f9bf61afa3a664a5b0be85e5a9074c
thehicksivist <hicks.a.james@gmail.com>
2019-02-01 14:21:17 +1000

    all fields successfully pass to the email except for the inspection date

--- routes/Routes.js
+++ routes/Routes.js
@@ -4,6 +4,7 @@ const mailer = require('../middleware/mailer');
 
 Router.post('/freequote', mailer, (res) => {
     console.log('Your expression of interest has been submitted successfully.')
+    res.send('Your request for a quote has been sent!')
 })
 
 module.exports = Router;
