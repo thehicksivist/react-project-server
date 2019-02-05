de0ff1d0c6d687a4fa7d24fed53e738cb47d0102
=jaouad Sourour <jaouad.pxy@gmail.com>
2019-02-05 12:50:27 +1000

    form container

--- client/src/components/Quote/forms/form.container.js
+++ client/src/components/Quote/forms/form.container.js
@@ -1,34 +1,54 @@
 import React from 'react';
 import FormComponent from './form.component';
 import axios from 'axios';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 
 const server = 'http://localhost:5000/freequote';
 
 class FormContainer extends React.Component {
-<<<<<<< HEAD
-	state = {
-		services: []
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			services: [],
+			firstName: '',
+			contactNumber: ''
+		};
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
+
+	handleInputChange(event) {
+		const target = event.target;
+		const value = target.value;
+		const name = target.name;
+
+		console.log(name);
+
+		this.setState({
+			[name]: value
+		});
+	}
 
 	submitForm = (event) => {
 		event.preventDefault();
-		console.log('submitting Form: ');
+		let formState = this.props.store.getState();
+		let formFields = formState.form['quote-form'].values;
+		console.log('submitting Form:', formFields);
+
 		// axios post from the client to the server/freequote
 		axios({
 			method: 'post',
 			url: `${server}`,
-			// form fields to be stored in state, then passed through from there
-			data: this.data,
-			config: { headers: { 'Content-Type': 'multipart/form-data' } }
+			data: formFields,
+			config: { headers: { 'Content-Type': 'application/json' } }
 		})
 			.then(function(response) {
 				//handle success
-				console.log(response);
+				console.log('axios post success', response);
 			})
 			.catch(function(response) {
 				//handle error
-				console.log(response);
+				console.log('axios post catch', formFields, response);
 			});
 	};
 
@@ -36,9 +56,6 @@ class FormContainer extends React.Component {
 		axios.get('http://localhost:5000/services').then((response) => {
 			this.setState({ services: response.data.map((x) => x.name) });
 		});
-		// axios get list from API
-
-		// set local state
 	}
 
 	render() {
@@ -46,75 +63,4 @@ class FormContainer extends React.Component {
 	}
 }
 
-const formConfiguration = {
-	form: 'my-very-own-form'
-};
-
-export default reduxForm(formConfiguration)(FormContainer);
-=======
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            services: [],
-            firstName: '',
-            contactNumber: ''
-        };
-    
-        this.handleInputChange = this.handleInputChange.bind(this);
-      }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        console.log(name)
-    
-        this.setState({
-          [name]: value
-        });
-    }
-
-    submitForm = (event) => {
-        event.preventDefault()
-        let formState = this.props.store.getState()
-        let formFields = formState.form['quote-form'].values
-        console.log('submitting Form:', formFields);
-           
-        // axios post from the client to the server/freequote
-        axios({
-            method: 'post',
-            url: `${server}`,
-            data: formFields,
-            config: { headers: {'Content-Type': 'application/json' }}
-            })
-            .then(function (response) {
-                //handle success
-                console.log('axios post success', response);
-            })
-            .catch(function (response) {
-                //handle error
-                console.log('axios post catch', formFields, response);
-            });
-      }
-
-    componentDidMount() {
-        axios.get('http://localhost:5000/services')
-        .then(response => {
-            this.setState({ services: response.data.map(x => x.name) })
-        })
-    }
-
-    render() {
-        return (
-            <FormComponent
-                handleSubmit={this.submitForm}
-                services={this.state.services}
-            />
-        )
-    }
-}
-
-export default FormContainer
->>>>>>> 3ed7876d39a897bc4587169a8f0d17e5b49ebf33
+export default FormContainer;
