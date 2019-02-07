d05b0f79deee3d76154bccf44727ecaa9bae63ae
thehicksivist <hicks.a.james@gmail.com>
2019-02-07 12:57:26 +1000

    fixed classNames on about

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -12,32 +12,32 @@ class About extends Component {
 			marginBottom: '10px'
 		};
 		return (
-			<div class="container mt-4 mb-4" style={{ height: '600px' }}>
+			<div className="container mt-4 mb-4" style={{ height: '600px' }}>
 				<div>
 					<img
 						style={imgStyle}
 						src="https://res.cloudinary.com/dvqlfwgtf/image/upload/v1548736044/Five%20Star/Troy_c.jpg"
-						class="rounded-circle"
+						className="rounded-circle"
 						alt=""
 					/>
 				</div>
-				<div class="p-4 pb-5 ">
-					<p class=" pb-2 lead">
+				<div className="p-4 pb-5 ">
+					<p className=" pb-2 lead">
 						Five Star Mowing offers fast and reliable lawn and garden care for commercial and domestic
 						resident properties in Caboolture and the surrounding areas of north Brisbane. After years of
 						experience what keeps Troy going is the rewarding opportunity to stand back and see what has
 						been accomplished. The satisfying feeling of ‘I did that!’ Even the smallest changes can make a
 						big difference to a yard.
 					</p>
-					<p class="pb-2 lead">
+					<p className="pb-2 lead">
 						So just fill out the{' '}
-						<a type="button" href="/freequote" class="btn btn-success ">
+						<a type="button" href="/freequote" className="btn btn-success ">
 							Quote Form
 						</a>{' '}
 						to have a free quote and property inspection.
 					</p>
 
-					<p class="container lead py-4">Because you’ve got better things to do on a weekend than mow!</p>
+					<p className="container lead py-4">Because you’ve got better things to do on a weekend than mow!</p>
 				</div>
 			</div>
 		);
