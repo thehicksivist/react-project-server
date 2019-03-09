0ac3841d4e889f75cdc7571374d04cf00052b071
thehicksivist <hicks.a.james@gmail.com>
2019-03-09 11:02:50 +1000

    removed client from server directory

--- client/src/components/Quote/form.validators.js
+++
@@ -1,7 +0,0 @@
-// validate : (value, allValues, props) => error [optional] #
-
-export const required = (value) => {
-    if (!value) {
-        return 'This field is required!'
-    }
-};
--- client/src/components/Quote/services.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default async function () {
-    let response = await axios.get('http://localhost:5000/services')
-    return response
-    //    return response.data.map(svc => svc.name)
-}
