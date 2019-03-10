4b82d11452ac5c8e428153751f1ab70e984ce8c3
thehicksivist <hicks.a.james@gmail.com>
2019-03-11 07:53:23 +1000

    altered configs to fix 404

--- server.js
+++ server.js
@@ -42,14 +42,14 @@ if (process.env.NODE_ENV === 'production') {
 	app.get;
 }
 
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-	// Set static folder
-	app.use(express.static('client/build'));
+// // Serve static assets if in production
+// if (process.env.NODE_ENV === 'production') {
+// 	// Set static folder
+// 	app.use(express.static('client/build'));
 
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-}
+// 	app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// 	});
+// }
 
 app.listen(process.env.PORT || 5000, () => console.log('Listening on http://localhost:5000'));
